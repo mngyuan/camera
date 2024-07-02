1b30988620bfa2de47485eb19db58e1b3e4d4b13
Kevin Lee <hikevinmlee@gmail.com>
2024-07-02 21:23:04 +0200

    use picamera2 instead of terminal

--- camera.py
+++ camera.py
@@ -1,15 +1,35 @@
+from picamera2 import Picamera2
 from gpiozero import Button
 from datetime import datetime
 from signal import pause
 import subprocess
 
-def say_hello():
-    subprocess.run(['rpicam-still', '--raw', '--output', f'/home/pi/camimages/{datetime.now():%Y-%m-%d-%H-%M-%S}.jpg'])
-    print(f'Saved to /home/pi/{datetime.now():%Y-%m-%d-%H-%M-%S}.jpg')
+def take_photo():
+    picam2.capture_file(f'/home/pi/picam2img/{datetime.now():%Y-%m-%d-%H-%M-%S}.jpg')
+    print(f'Saved to /home/pi/picam2img/{datetime.now():%Y-%m-%d-%H-%M-%S}.jpg')
 
 button = Button("BOARD10", pull_up=False)
 
-button.when_pressed = say_hello
+button.when_pressed = take_photo
+
+picam2 = Picamera2()
+preview_config = picam2.create_still_configuration()
+
+# Get the camera configuration
+config = picam2.create_still_configuration()
+controls = config["controls"]
+
+# Disable AE and AWB
+controls["AeEnable"] = False
+controls["AwbEnable"] = False
+
+# Set manual controls (if needed)
+controls["ExposureTime"] = 10000  # in microseconds
+controls["AnalogueGain"] = 1.0
+controls["ColourGains"] = (1.5, 1.2)  # Red, Blue gains
+
+# Apply the configuration
+picam2.configure(config)
+picam2.start()
 
 pause()
-
